e7c8ed3b63d0394224846733c85e3864dc319a33
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-01-08 09:56:03 -0500

    Better support for code blocks. Fixed a bug in header formatting.

--- coqdoc.py
+++ coqdoc.py
@@ -1,4 +1,6 @@
-from bs4 import BeautifulSoup
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup, NavigableString
 import re
 import shlex
 import os.path
@@ -8,10 +10,17 @@ def fix_title(elem, h):
     hdr = elem.find(h, 'section')
     if hdr is not None:
         lbl = hdr.find_previous_sibling('a').extract()
-        hdr = hdr.extract()
-        elem.insert_before(hdr)
+        del hdr['class']
+        #hdr = hdr.extract()
+        #elem.insert_before(hdr)
         hdr.wrap(lbl)
 
+
+def replace_with(old_elem, new_elem):
+    if isinstance(new_elem, str):
+        new_elem = NavigableString(new_elem)
+    old_elem.replace_with(new_elem)
+    return new_elem
 
 def fix_tufte(fdin):
     """
@@ -25,8 +34,10 @@ def fix_tufte(fdin):
     data = re.sub(r'<div class="code">\n+<br/>', '<div class="code">', data, flags=re.MULTILINE)
     data = re.sub(r'<div class="code">\n+(<br\/>)?', '<div class="code">', data, flags=re.MULTILINE)
     data = re.sub(r'<div class="code"></div>', '', data)
+    data = re.sub(r'(<br/>\n*)+', r'<br/>', data, flags=re.MULTILINE)
     bs = BeautifulSoup(data, 'html.parser')
     page = bs.find('div', id='page')
+
     if page is not None:
         page.name = 'article'
         del page['id']
@@ -38,15 +49,38 @@ def fix_tufte(fdin):
         del main['id']
 
     for elem in bs.find_all('div', 'doc'):
-        for h in ('h1', 'h2', 'h3'):
-            fix_title(elem, h)
-
+        fix_title(elem, 'h1')
+        fix_title(elem, 'h2')
         elem.name = 'p'
+        for txt in elem.contents:
+            if not isinstance(txt, NavigableString): continue
+            txt = replace_with(txt, str(txt).strip())
+            splits = re.split(r'([ \t]*\n){2,}', str(txt), flags=re.MULTILINE)
+            splits = list(x for x in splits if x.strip() != "")
+            if len(splits) >= 2:
+                part, *rest = splits
+                txt = replace_with(txt, part)
+                for fragment in rest:
+                    p = bs.new_tag('br')
+                    p['class'] = "NL"
+                    txt.insert_after(p)
+                    txt = NavigableString(fragment)
+                    p.insert_after(txt)
+            
+        for div in elem.find_all('pre'):
+            p = bs.new_tag('br')
+            p['class'] = "PRE"
+            div.insert_after(p)
+            
         del elem['class']
 
     for elem in bs.find_all('div', 'code'):
         elem.name = 'pre'
-        del elem['class']
+        #del elem['class']
+
+    for elem in bs.find_all('span', 'inlinecode'):
+        elem.insert_before(NavigableString(' '))
+        elem.insert_after(NavigableString(' '))
 
     mark = bs.find(text=re.compile('^This page has been generated'))
     if mark is not None:
@@ -58,7 +92,17 @@ def fix_tufte(fdin):
         if elem.text.strip() == "":
             elem.decompose()
 
-    return bs
+    #return bs
+    data = re.sub(r'<br class="NL"/>', r'</p><p>', str(bs), flags=re.MULTILINE)
+    data = data.replace(r'<tt>', r'<code>')
+    data = data.replace(r'</tt>', r'</code>')
+    data = re.sub(r'<a name="[a-z0-9]+"><h[1-9]>', lambda x: r'</p>' + x.group(0), data, flags=re.MULTILINE)
+    data = re.sub(r'</h[1-9]></a>', lambda x: x.group(0) + r'<p>', data, flags=re.MULTILINE)
+    data = data.replace(r'<p></p>', '')
+    data = data.replace(r'<pre>', '</p><pre>')
+    data = data.replace(r'</pre><br class="PRE"/>', '</pre><p>')
+    return data
+    #return BeautifulSoup(data, 'html.parser').prettify()
 
 def fix(filename):
     """
@@ -71,7 +115,7 @@ def fix(filename):
 layout: default
 ---
 """)
-        fp.write(str(bs.prettify()))
+        fp.write(str(bs))
 
 class Project:
     """
@@ -114,7 +158,7 @@ class Project:
             coq_files = list(x.strip() for x in lines[1:] if x.strip() != "" and not x.strip().startswith("#")),
         )
 
-DEFAULT_ARGS = "--body-only --no-index --lib-subtitles -s -g -l"
+DEFAULT_ARGS = "--body-only --no-index --lib-subtitles -s"
 
 def main():
     import sys, argparse
@@ -122,15 +166,28 @@ def main():
     d_target_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
     parser.add_argument("-d", default=d_target_dir, dest="target_dir", help="Target directory. Default: %(default)s")
     parser.add_argument("-p", default="_CoqProject", dest="project_file", help="Coq Project file. Default: %(default)s")
+    pass_through = [
+        ("-l", "light mode (only defs and statements)"),
+        ("-g", "(gallina) skip proofs"),
+    ]
+    for (param, help_desc) in pass_through:
+        parser.add_argument(param,
+            action="store_const",
+            const=[param], default=[],
+            help=help_desc)
+    parser.add_argument("--extra", dest="extra", nargs="*", default=shlex.split(DEFAULT_ARGS), help="Extra arguments. Default: %(default)s")
     args = parser.parse_args()
+    
+    extra_args = args.extra
+    
+    for (param, _) in pass_through:
+        if getattr(args, param[1:]):
+            extra_args.append(param)
 
     prj = Project.load(args.project_file)
-
-    subprocess.call(
-        ["coqdoc"] + prj.args +
-        shlex.split(DEFAULT_ARGS) +
-        ["-d", args.target_dir] +
-        prj.coq_files)
+    cmd = ["coqdoc"] + prj.args + ["-d", args.target_dir] + extra_args + prj.coq_files
+    print(" ".join(cmd))
+    subprocess.call(cmd)
     os.unlink(os.path.join(args.target_dir, "coqdoc.css"))
 
     for fname in prj.html_files:
